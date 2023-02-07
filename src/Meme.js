642e9f91d66d2924d0f3062e80226059e7f2c2dc
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-02-07 11:17:05 +0100

    Getting Meme Image

--- src/Meme.js
+++ src/Meme.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css"
+import memesData from "./memesData";
 
 
-function Meme(){
-  return(
+export default function Meme() {
+
+  const [memeImage, setMemeImage] = React.useState("");
+
+
+  function getMemeImage(){
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMemeImage (memesArray[randomNumber].url)
+    
+
+    console.log(url)
+  }
+  return (
     <main className="main">
-      <form className="form">
-        <input 
-        type="text"
-        placeholder="Top Text" 
-        className="form-input"
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top Text"
+          className="form-input"
         />
-        <input 
-        type="text"
-        placeholder="Bottom Text" 
-        className="form-input"
+        <input
+          type="text"
+          placeholder="Bottom Text"
+          className="form-input"
         />
-        <button className="form-button">Get a new meme image</button>
-      </form>
+        <button 
+        className="form-button"
+        onClick={getMemeImage}
+        >Get a new meme image</button>
+      </div>
+      <div className="meme-image-wrapper"><img className="meme-image" src={memeImage} alt="Meme Img"/></div>
+      
     </main>
   )
 }
-
-export default Meme;
